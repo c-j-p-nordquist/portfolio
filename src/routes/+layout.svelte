522bf43d751948ba17776f650470a42cf20c55a1
c-j-p-nordquist <philip@nordquist.me>
2026-04-13 08:13:06 +0200

    fix fade

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,7 +21,7 @@
 <!-- Subtle noise texture overlay -->
 <div 
 	class="pointer-events-none fixed inset-0 z-50 h-full w-full opacity-[0.02] dark:opacity-[0.04]" 
-	style="background-image: url(&quot;data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='1.2' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E&quot;);"
+	style="background-image: url(&quot;data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='1.2' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E&quot;); view-transition-name: noise-overlay;"
 ></div>
 
 <div class="min-h-screen flex flex-col relative z-0">
